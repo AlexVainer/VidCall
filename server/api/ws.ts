d3b229a27f0e2c48c44e6cfb6b5bb182c928fb82
AlexVainer <john.black.bee@gmail.com>
2025-12-31 00:52:37 +0700

    session added

--- server/api/ws.ts
+++ server/api/ws.ts
@@ -1,9 +1,10 @@
 import { WebSocket } from "ws";
 
 export const wsHandler = (ws: WebSocket) => {
-  console.log('New client connected');
+  console.log('New client connected')
 
   ws.on('message', (message: string) => {
     console.log(`Received message => ${message}`)
+    ws.send(`Server received: ${message}`)
   })
 }
